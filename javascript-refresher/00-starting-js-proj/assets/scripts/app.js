9fe1b07a5e664e2bdbd5b29ac54b39093a42255b
Giuseppe Eustaquio <gseppe222@gmail.com>
2024-09-14 01:36:18 +0800

    adds assets, and import export javascript file

---
+++ javascript-refresher/00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,14 @@
+// import named
+// import { apiKey } from "./util.js";
+// console.log(util.apiKey);
+
+// import multiple named
+import { apiKey, abc as content } from "./util.js";
+console.log(apiKey, content);
+
+// import all as javascript object
+// import * as util from "./util.js";
+// console.log(util.abc);
+
+// import default
+// import apiKey from "./util.js";
---
+++ javascript-refresher/00-starting-js-proj/assets/scripts/util.js
@@ -0,0 +1,6 @@
+// export named
+export let apiKey = "zxzxzxzxzxzxzxz";
+export let abc = "ccccccc";
+
+// export default
+// export default "xcxccxcxcxcx";
