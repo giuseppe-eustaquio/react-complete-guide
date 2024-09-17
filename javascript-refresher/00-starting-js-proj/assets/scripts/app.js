0135fd535df9709973dc346de0e43284b92f5fd4
Giuseppe Eustaquio <gseppe222@gmail.com>
2024-09-18 01:01:47 +0800

    adds functions, arrays, and objects

--- javascript-refresher/00-starting-js-proj/assets/scripts/app.js
+++ javascript-refresher/00-starting-js-proj/assets/scripts/app.js
@@ -3,8 +3,8 @@
 // console.log(util.apiKey);
 
 // import multiple named
-import { apiKey, abc as content } from "./util.js";
-console.log(apiKey, content);
+// import { apiKey, abc as content } from "./util.js";
+// console.log(apiKey, content);
 
 // import all as javascript object
 // import * as util from "./util.js";
@@ -12,3 +12,39 @@ console.log(apiKey, content);
 
 // import default
 // import apiKey from "./util.js";
+
+// sample function
+const combine = function (a, b, c) {
+  return (a * b) / c;
+};
+
+const hobbies = ["sports", "cooking", "reading"];
+
+hobbies.push("working");
+
+// const index = hobbies.findIndex((item) => {
+//   return item === "sports";
+// });
+const index = hobbies.findIndex((item) => item === "sports");
+
+const surprisedHobbies = hobbies.map((item) => item + "!");
+const objectHobbies = hobbies.map((item) => ({ text: item }));
+
+const transformToObjects = function (numberArray) {
+  return numberArray.map((num) => ({ val: num }));
+};
+
+// console.log(transformToObjects([1, 2, 3]));
+
+// destructure array
+
+const [firstName, lastName] = ["Abcd", "Efgh"];
+// console.log(firstName, lastName);
+
+// destructure objects
+const { name, age } = {
+  name: "Abcd",
+  age: 99,
+};
+
+console.log(name, age);
